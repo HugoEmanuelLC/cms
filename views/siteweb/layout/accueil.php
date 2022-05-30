d581d5ca3fb2dafe02442251ee493073cca9afd8
hugo clavinas <helc85.info@gmail.com>
2022-05-30 13:06:03 +0200

    v-1.0.0

--- views/siteweb/layout/accueil.php
+++ views/siteweb/layout/accueil.php
@@ -13,26 +13,35 @@ $descriptitle = 'Accueil';
 </header>
 
 <div class="intitule">
-    <ul>
-        <li><h1>Developeur</h1></li>
-        <li><h2>web apps</h2></li>
-        <li><h3>junior</h3></li>
-        <li><button>C.V.</button></li>
-    </ul>
+    <div class="text">
+        <ul>
+            <li><h1>Developeur</h1></li>
+            <li><h2>web apps</h2></li>
+            <li><h3>junior</h3></li>
+            <li><button>C.V.</button></li>
+        </ul>
+    </div>
+
+    <div class="photoProfil">
+        <div class="shadow"></div>
+    </div>
 </div>
 
 <div class="contenue">
+
+    <h1 class="titres">Demos:</h1>
+
     <div class="demos">
-        <div class="demo">
-            <a href=""><i class="far fa-plus-circle"></i></a>
+        <div class="demo" style="background-image: url('https://wallpaperaccess.com/full/112486.jpg');">
+            <a href=""><i class="fas fa-plus"></i></a>
         </div>
 
-        <div class="demo">
-            <a href=""><i class="far fa-plus-circle"></i></a>
+        <div class="demo" style="background-image: url('https://images.unsplash.com/photo-1612821969256-55c71d64c245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXBob25lJTIwd2FsbHBhcGVyJTIwaGR8ZW58MHx8MHx8&w=1000&q=80');">
+            <a href=""><i class="fas fa-plus"></i></a>
         </div>
 
-        <div class="demo">
-            <a href=""><i class="far fa-plus-circle"></i></a>
+        <div class="demo" style="background-image: url('https://cdn.wallpapersafari.com/14/87/lwnYvT.jpg');">
+            <a href=""><i class="fas fa-plus"></i></a>
         </div>
     </div>
 
